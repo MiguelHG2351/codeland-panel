a87e3781b6e3c9fd9f8fb0b56b85dd118c2dc8be
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2021-09-12 13:55:02 -0600

    problema con la versión 5 de mongodb

--- pages/api/getUser.js
+++ pages/api/getUser.js
@@ -1,28 +1,37 @@
 import Mongo from '../../libs/Mongo';
 import userModel from "../../models/users";
-import projectsModel from "../../models/project";
 
 const actions = {
     async getAllUsers() {
-        return  await userModel.aggregate([
-            {
-                $lookup: {
-                    from: "projects",
-                    localField: "_id",
-                    foreignField: "users_id",
-                    as: "projects"
+        return  await userModel.aggregate(
+            [
+                {
+                    $lookup: {
+                        from: "projects",
+                        localField: "_id",
+                        foreignField: "users_id",
+                        as: "projects"
+                    },
                 },
-            },
-            {
-                $match: {}
-            }
-        ])
-    },
+                {
+                    $group: {
+                        _id: "$_id",
+                        username: {$first: "$username"},
+                        email: {$first: "$email"},
+                        projects: {$push: "$projects"},
+                        cover: {$first: "$cover"},
+                        created_at: {$first: "$created_at"},
+                    }
+                }
+            ]
+        )
+    }
 }
 
 export default async function(req, res) {
     new Mongo();
     const user = await actions.getAllUsers();
+    console.log(user);
 
     return res.json({
         user,
--- pages/index.js
+++ pages/index.js
@@ -21,25 +21,29 @@ export default function Home() {
                     type="image/x-icon"
                 />
             </Head>
-            <div className="md:grid grid-cols-3 gap-3 w-11/12 m-auto">
-                {data.user.map((item, index) => (
-                    <div
-                        className="relative overflow-hidden flex items-end border mt-7 md:mt-0 border-gray-600 dark:bg-purple-600 h-80 rounded-lg shadow-md cursor-pointer transition-transform transform hover:scale-90"
-                        key={index}
-                    >
-                        <img
-                            src={item.cover}
-                            className="cover absolute top-0 left-0 w-full h-full z-1- object-cover"
-                            alt={item.username}
-                            loading="lazy"
-                        />
-                        <div className="user-action text-white p-3 w-full bg-purple-700">
-                            <h2>{item.username}</h2>
-                            <p>Projectos: {item.projects.length}</p>
+            <main>
+                <input type="text" className="p-2" placeholder="Buscar usuario" />
+                <div className="md:grid grid-cols-3 gap-3 w-11/12 m-auto">
+                    {data.user.map((item, index) => (
+                        <div
+                            className="relative overflow-hidden flex items-end border mt-7 md:mt-0 border-gray-600 dark:bg-purple-600 h-80 rounded-lg shadow-md cursor-pointer transition-transform transform hover:scale-90"
+                            key={index}
+                        >
+                            <img
+                                src={item.cover}
+                                className="cover absolute top-0 left-0 w-full h-full z-1- object-cover"
+                                alt={item.username}
+                                loading="lazy"
+                            />
+                            <div className="user-action text-white p-3 w-full bg-purple-700">
+                                <h2>{item.username}</h2>
+                                <p>Projectos: {item.projects.length}</p>
+                                <p>Ingreso: {new Date(item.created_at).toLocaleString("en-us", { weekday: "short", day: 'numeric', month: 'short' })}</p>
+                            </div>
                         </div>
-                    </div>
-                ))}
-            </div>
+                    ))}
+                </div>
+            </main>
         </>
     );
 }
